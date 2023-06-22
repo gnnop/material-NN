ad31a5607bd11b826727c566bb2c36e99bab4f67
gnnop <gavin.nop@gmail.com>
2023-06-22 15:59:03 -0500

    preprocessing should work out of box now

--- ccnn-ml.py
+++ ccnn-ml.py
@@ -27,9 +27,10 @@ import time
 Not synced up. Make changes at the same time
 """
 maxDims = 48#Number of cells 60 atom max. cubic root is 4. *2 for space =8, *2.5 for tesselation is 20 *2 (arbitrary) for 40-1.6MB
-conversionFactor = 3
+conversionFactor = 2.7#Always scale the maxDims with the conversionFactor
 #need to be able to rep atoms, probably have 3* max unit cell
 maxRep = 7 + 16 + 1 + 3 + 1 #3 - atomic distance, 1 - unit cell mask
+dims = (maxDims, maxDims, maxDims, maxRep)
 centre = np.array([maxDims / 2, maxDims / 2, maxDims / 2])
 
 #Converts stuff to stuff.
