88ca514b70f4035fcd4cfeb9ad648a4021e3ff49
m516 <mmundy@iastate.edu>
2023-08-07 19:46:39 -0500

    backwards compatibility with Python 3.9

--- _common_data_preprocessing.py
+++ _common_data_preprocessing.py
@@ -50,246 +50,245 @@ completeTernary = list(itertools.product([-1, 0, 1], repeat=3))
 #also, I'm just going to chuck the bottom row
 #of the table, no one wants those elements
 def atomToCoords(name):
-    match name:
-        case "H":
-            return [0, 0, 0]
-        case "He":
-            return [0, 0, 1]#different, i likey here
-        case "Li":
-            return [1, 0, 0]
-        case "Be":
-            return [1, 0, 1]
-        case "B":
-            return [1, 6, 0]
-        case "C":
-            return [1, 6, 1]
-        case "N":
-            return [1, 7, 0]
-        case "O":
-            return [1, 7, 1]
-        case "F":
-            return [1, 8, 0]
-        case "Ne":
-            return [1, 8, 1]
-        case "Na":
-            return [2, 0, 0]
-        case "Mg":
-            return [2, 0, 1]
-        case "Al":
-            return [2, 6, 0]
-        case "Si":
-            return [2, 6, 1]
-        case "P":
-            return [2, 7, 0]
-        case "S":
-            return [2, 7, 1]
-        case "Cl":
-            return [2, 8, 0]
-        case "Ar":
-            return [2, 8, 1]
-        case "K":
-            return [3, 0, 0]
-        case "Ca":
-            return [3, 0, 1]
-        case "Sc":
-            return [3, 1, 0]
-        case "Ti":
-            return [3, 1, 1]
-        case "V":
-            return [3, 2, 0]
-        case "Cr":
-            return [3, 2, 1]
-        case "Mn":
-            return [3, 3, 0]
-        case "Fe":
-            return [3, 3, 1]
-        case "Co":
-            return [3, 4, 0]
-        case "Ni":
-            return [3, 4, 1]
-        case "Cu":
-            return [3, 5, 0]
-        case "Zn":
-            return [3, 5, 1]
-        case "Ga":
-            return [3, 6, 0]
-        case "Ge":
-            return [3, 6, 1]
-        case "As":
-            return [3, 7, 0]
-        case "Se":
-            return [3, 7, 1]
-        case "Br":
-            return [3, 8, 0]
-        case "Kr":
-            return [3, 8, 1]
-        case "Rb":
-            return [4, 0, 0]
-        case "Sr":
-            return [4, 0, 1]
-        case "Y":
-            return [4, 1, 0]
-        case "Zr":
-            return [4, 1, 1]
-        case "Nb":
-            return [4, 2, 0]
-        case "Mo":
-            return [4, 2, 1]
-        case "Tc":
-            return [4, 3, 0]
-        case "Ru":
-            return [4, 3, 1]
-        case "Rh":
-            return [4, 4, 0]
-        case "Pd":
-            return [4, 4, 1]
-        case "Ag":
-            return [4, 5, 0]
-        case "Cd":
-            return [4, 5, 1]
-        case "In":
-            return [4, 6, 0]
-        case "Sn":
-            return [4, 6, 1]
-        case "Sb":
-            return [4, 7, 0]
-        case "Te":
-            return [4, 7, 1]
-        case "I":
-            return [4, 8, 0]
-        case "Xe":
-            return [4, 8, 1]
-        case "Cs":
-            return [5, 0, 0]
-        case "Ba":
-            return [5, 0, 1]
-        case "Lu":
-            return [5, 1, 0]
-        case "Hf":
-            return [5, 1, 1]
-        case "Ta":
-            return [5, 2, 0]
-        case "W":
-            return [5, 2, 1]
-        case "Re":
-            return [5, 3, 0]
-        case "Os":
-            return [5, 3, 1]
-        case "Ir":
-            return [5, 4, 0]
-        case "Pt":
-            return [5, 4, 1]
-        case "Au":
-            return [5, 5, 0]
-        case "Hg":
-            return [5, 5, 1]
-        case "Tl":
-            return [5, 6, 0]
-        case "Pb":
-            return [5, 6, 1]
-        case "Bi":
-            return [5, 7, 0]
-        case "Po":
-            return [5, 7, 1]
-        case "At":
-            return [5, 8, 0]
-        case "Rn":
-            return [5, 8, 1]
-        case "Fr":
-            return [6, 0, 0]
-        case "Ra":
-            return [6, 0, 1]
-        case "Lr":
-            return [6, 1, 0]
-        case "Rf":
-            return [6, 1, 1]
-        case "Db":
-            return [6, 2, 0]
-        case "Sg":
-            return [6, 2, 1]
-        case "Bh":
-            return [6, 3, 0]
-        case "Hs":
-            return [6, 3, 1]
-        case "Mt":
-            return [6, 4, 0]
-        case "Ds":
-            return [6, 4, 1]
-        case "Rg":
-            return [6, 5, 0]
-        case "Cn":
-            return [6, 5, 1]
-        case "Nh":
-            return [6, 6, 0]
-        case "Fl":
-            return [6, 6, 1]
-        case "Mc":
-            return [6, 7, 0]
-        case "Lv":
-            return [6, 7, 1]
-        case "Ts":
-            return [6, 8, 0]
-        case "Og":
-            return [6, 8, 1]
-        case "La":
-            return [5, 9, 0]
-        case "Ce":
-            return [5, 9, 1]
-        case "Pr":
-            return [5, 10, 0]
-        case "Nd":
-            return [5, 10, 1]
-        case "Pm":
-            return [5, 11, 0]
-        case "Sm":
-            return [5, 11, 1]
-        case "Eu":
-            return [5, 12, 0]
-        case "Gd":
-            return [5, 12, 1]
-        case "Tb":
-            return [5, 13, 0]
-        case "Dy":
-            return [5, 13, 1]
-        case "Ho":
-            return [5, 14, 0]
-        case "Er":
-            return [5, 14, 1]
-        case "Tm":
-            return [5, 15, 0]
-        case "Yb":
-            return [5, 15, 1]
-        case "Ac":
-            return [6, 9, 0]
-        case "Th":
-            return [6, 9, 1]
-        case "Pa":
-            return [6, 10, 0]
-        case "U":
-            return [6, 10, 1]
-        case "Np":
-            return [6, 11, 0]
-        case "Pu":
-            return [6, 11, 1]
-        case "Am":
-            return [6, 12, 0]
-        case "Cm":
-            return [6, 12, 1]
-        case "Bk":
-            return [6, 13, 0]
-        case "Cf":
-            return [6, 13, 1]
-        case "Es":
-            return [6, 14, 0]
-        case "Fm":
-            return [6, 14, 1]
-        case "Md":
-            return [6, 15, 0]
-        case "No":
-            return [6, 15, 1]
-        case _:
-            print(name)
-            exit()
+    if name == "H":
+        return [0, 0, 0]
+    elif name == "He":
+        return [0, 0, 1]#different, i likey here
+    elif name == "Li":
+        return [1, 0, 0]
+    elif name == "Be":
+        return [1, 0, 1]
+    elif name == "B":
+        return [1, 6, 0]
+    elif name == "C":
+        return [1, 6, 1]
+    elif name == "N":
+        return [1, 7, 0]
+    elif name == "O":
+        return [1, 7, 1]
+    elif name == "F":
+        return [1, 8, 0]
+    elif name == "Ne":
+        return [1, 8, 1]
+    elif name == "Na":
+        return [2, 0, 0]
+    elif name == "Mg":
+        return [2, 0, 1]
+    elif name == "Al":
+        return [2, 6, 0]
+    elif name == "Si":
+        return [2, 6, 1]
+    elif name == "P":
+        return [2, 7, 0]
+    elif name == "S":
+        return [2, 7, 1]
+    elif name == "Cl":
+        return [2, 8, 0]
+    elif name == "Ar":
+        return [2, 8, 1]
+    elif name == "K":
+        return [3, 0, 0]
+    elif name == "Ca":
+        return [3, 0, 1]
+    elif name == "Sc":
+        return [3, 1, 0]
+    elif name == "Ti":
+        return [3, 1, 1]
+    elif name == "V":
+        return [3, 2, 0]
+    elif name == "Cr":
+        return [3, 2, 1]
+    elif name == "Mn":
+        return [3, 3, 0]
+    elif name == "Fe":
+        return [3, 3, 1]
+    elif name == "Co":
+        return [3, 4, 0]
+    elif name == "Ni":
+        return [3, 4, 1]
+    elif name == "Cu":
+        return [3, 5, 0]
+    elif name == "Zn":
+        return [3, 5, 1]
+    elif name == "Ga":
+        return [3, 6, 0]
+    elif name == "Ge":
+        return [3, 6, 1]
+    elif name == "As":
+        return [3, 7, 0]
+    elif name == "Se":
+        return [3, 7, 1]
+    elif name == "Br":
+        return [3, 8, 0]
+    elif name == "Kr":
+        return [3, 8, 1]
+    elif name == "Rb":
+        return [4, 0, 0]
+    elif name == "Sr":
+        return [4, 0, 1]
+    elif name == "Y":
+        return [4, 1, 0]
+    elif name == "Zr":
+        return [4, 1, 1]
+    elif name == "Nb":
+        return [4, 2, 0]
+    elif name == "Mo":
+        return [4, 2, 1]
+    elif name == "Tc":
+        return [4, 3, 0]
+    elif name == "Ru":
+        return [4, 3, 1]
+    elif name == "Rh":
+        return [4, 4, 0]
+    elif name == "Pd":
+        return [4, 4, 1]
+    elif name == "Ag":
+        return [4, 5, 0]
+    elif name == "Cd":
+        return [4, 5, 1]
+    elif name == "In":
+        return [4, 6, 0]
+    elif name == "Sn":
+        return [4, 6, 1]
+    elif name == "Sb":
+        return [4, 7, 0]
+    elif name == "Te":
+        return [4, 7, 1]
+    elif name == "I":
+        return [4, 8, 0]
+    elif name == "Xe":
+        return [4, 8, 1]
+    elif name == "Cs":
+        return [5, 0, 0]
+    elif name == "Ba":
+        return [5, 0, 1]
+    elif name == "Lu":
+        return [5, 1, 0]
+    elif name == "Hf":
+        return [5, 1, 1]
+    elif name == "Ta":
+        return [5, 2, 0]
+    elif name == "W":
+        return [5, 2, 1]
+    elif name == "Re":
+        return [5, 3, 0]
+    elif name == "Os":
+        return [5, 3, 1]
+    elif name == "Ir":
+        return [5, 4, 0]
+    elif name == "Pt":
+        return [5, 4, 1]
+    elif name == "Au":
+        return [5, 5, 0]
+    elif name == "Hg":
+        return [5, 5, 1]
+    elif name == "Tl":
+        return [5, 6, 0]
+    elif name == "Pb":
+        return [5, 6, 1]
+    elif name == "Bi":
+        return [5, 7, 0]
+    elif name == "Po":
+        return [5, 7, 1]
+    elif name == "At":
+        return [5, 8, 0]
+    elif name == "Rn":
+        return [5, 8, 1]
+    elif name == "Fr":
+        return [6, 0, 0]
+    elif name == "Ra":
+        return [6, 0, 1]
+    elif name == "Lr":
+        return [6, 1, 0]
+    elif name == "Rf":
+        return [6, 1, 1]
+    elif name == "Db":
+        return [6, 2, 0]
+    elif name == "Sg":
+        return [6, 2, 1]
+    elif name == "Bh":
+        return [6, 3, 0]
+    elif name == "Hs":
+        return [6, 3, 1]
+    elif name == "Mt":
+        return [6, 4, 0]
+    elif name == "Ds":
+        return [6, 4, 1]
+    elif name == "Rg":
+        return [6, 5, 0]
+    elif name == "Cn":
+        return [6, 5, 1]
+    elif name == "Nh":
+        return [6, 6, 0]
+    elif name == "Fl":
+        return [6, 6, 1]
+    elif name == "Mc":
+        return [6, 7, 0]
+    elif name == "Lv":
+        return [6, 7, 1]
+    elif name == "Ts":
+        return [6, 8, 0]
+    elif name == "Og":
+        return [6, 8, 1]
+    elif name == "La":
+        return [5, 9, 0]
+    elif name == "Ce":
+        return [5, 9, 1]
+    elif name == "Pr":
+        return [5, 10, 0]
+    elif name == "Nd":
+        return [5, 10, 1]
+    elif name == "Pm":
+        return [5, 11, 0]
+    elif name == "Sm":
+        return [5, 11, 1]
+    elif name == "Eu":
+        return [5, 12, 0]
+    elif name == "Gd":
+        return [5, 12, 1]
+    elif name == "Tb":
+        return [5, 13, 0]
+    elif name == "Dy":
+        return [5, 13, 1]
+    elif name == "Ho":
+        return [5, 14, 0]
+    elif name == "Er":
+        return [5, 14, 1]
+    elif name == "Tm":
+        return [5, 15, 0]
+    elif name == "Yb":
+        return [5, 15, 1]
+    elif name == "Ac":
+        return [6, 9, 0]
+    elif name == "Th":
+        return [6, 9, 1]
+    elif name == "Pa":
+        return [6, 10, 0]
+    elif name == "U":
+        return [6, 10, 1]
+    elif name == "Np":
+        return [6, 11, 0]
+    elif name == "Pu":
+        return [6, 11, 1]
+    elif name == "Am":
+        return [6, 12, 0]
+    elif name == "Cm":
+        return [6, 12, 1]
+    elif name == "Bk":
+        return [6, 13, 0]
+    elif name == "Cf":
+        return [6, 13, 1]
+    elif name == "Es":
+        return [6, 14, 0]
+    elif name == "Fm":
+        return [6, 14, 1]
+    elif name == "Md":
+        return [6, 15, 0]
+    elif name == "No":
+        return [6, 15, 1]
+    elif name == _:
+        print(name)
+        exit()
 
 def serializeAtom(atom, poscar, index):
     atomRep = atomToCoords(atom)
@@ -384,26 +383,26 @@ def serializeSpaceGroup(num):
 def convertTopoToIndex(rows, topo):
     if topo == "f":
         arr = [0.0]*5
-        match rows[5].strip():
-            case "ES":
-                arr[0] = 1.0
-            case "ESFD":
-                arr[1] = 1.0
-            case "NLC":
-                arr[2] = 1.0
-            case "SEBR":
-                arr[3] = 1.0
-            case "LCEBR":
-                arr[4] = 1.0
+        r = rows[5].strip()
+        if r == "ES":
+            arr[0] = 1.0
+        elif r == "ESFD":
+            arr[1] = 1.0
+        elif r == "NLC":
+            arr[2] = 1.0
+        elif r == "SEBR":
+            arr[3] = 1.0
+        elif r == "LCEBR":
+            arr[4] = 1.0
     else:
         arr = [0.0]*3
-        match rows[4].strip():
-            case "trivial":
-                arr[0] = 1.0
-            case "TI":
-                arr[1] = 1.0
-            case "SM":
-                arr[2] = 1.0
+        r = rows[4].strip();
+        if r == "trivial":
+            arr[0] = 1.0
+        elif r == "TI":
+            arr[1] = 1.0
+        elif r == "SM":
+            arr[2] = 1.0
     return arr
 
 maxSize = 60
