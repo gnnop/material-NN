dc041ab92dd032f1a8289e35327f96739ff75b4e
gnnop <gavin.nop@gmail.com>
2023-10-16 17:37:38 -0500

    Messy file incoming. CCNN appears to be functional

--- ccnn_ml.py
+++ ccnn_ml.py
@@ -1,3 +1,4 @@
+import timeit
 from _common_data_preprocessing import *
 from _common_ml import *
 import functools
@@ -11,7 +12,7 @@ import pickle
 import numpy as np
 from typing import Any, List, Tuple
 import time
-from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import Pool, Manager
 import jax.numpy as jnp
 import haiku as hk
 import itertools
@@ -21,6 +22,13 @@ sys.path.append( os.curdir )
 from ccnn_config import *
 from ccnn_shared import *
 import concurrent.futures
+
+if globals["dataSize"] == 14:
+  sym = "f"
+elif globals["dataSize"] == 38:
+  sym = "c"
+else:
+  sym = ""
 
 def givenPointDetermineCubeAndOverlap(position):
     index = np.round(position).astype(int)
@@ -85,7 +93,7 @@ def testConvexity(index, axes, invAxes):
   return c if 1 else 0
 
 
-def prep_data(row):
+def prep_data(row, symi):#sym is passed in to avoid globals strangeness with other threads
   #atoms
   denseEncode = np.zeros((maxDims, maxDims, maxDims, maxRep))
   
@@ -126,16 +134,7 @@ def prep_data(row):
 
   #Finally, fill in the tiled global values:
   #get symm back. I'm trying to make this similar to the other ML programs.
-  #However, because I'm now expanding things on the fly, I need to use the symmetry and this is the least invasive
-  #way to do that:
-  if globals["dataSize"] == 14:
-    sym = "f"
-  elif globals["dataSize"] == 38:
-    sym = "c"
-  else:
-    sym = ""
-  
-  globs = np.tile(np.array(getGlobalData(poscar, row, sym)), ((maxDims, maxDims, maxDims, 1)))
+  globs = np.tile(np.array(getGlobalData(poscar, row, symi)), ((maxDims, maxDims, maxDims, 1)))
 
   space = np.concatenate((denseEncode, mask, globs), axis=-1)
 
@@ -155,14 +154,13 @@ def evaluate(dataset: List[Any],
   # Get a candidate graph and label to initialize the network.
 
   #This should get a timer statement too
-  #print(dataset)
   accumulated_loss = 0
   accumulated_accuracy = 0
   compute_loss_fn = jax.jit(functools.partial(compute_loss, net=net))
   for idx in range(len(dataLabels)):
     obj = dataset[idx]#graph
     label = dataLabels[idx]#label
-    temp_objs = np.array(list(map(prep_data, [obj])))
+    temp_objs = np.array(list(map(lambda a: prep_data(a, sym), [obj])))
     temp_labels = jnp.array([label])
     # loss, acc = compute_loss(params, temp_objs, temp_labels, net=net)
     loss, acc = compute_loss_fn(params, temp_objs, temp_labels)
@@ -174,25 +172,26 @@ def evaluate(dataset: List[Any],
   print(f'Eval loss: {loss}, accuracy {accuracy}')
   return loss, accuracy
 
+#The following functions are wrappers for the multiprocessing modules to play with:
+def ordered_process(index,data, sym):
+    smoething = prep_data(data, sym)
+    return index, smoething
+
 def main(obj):
-    print("initializing")
+    print("initializing", flush=True)
 
     #test pool based multithreading later to see if there's
     #a speed up
 
-    pool = ThreadPool(20)
+    pool = Pool(processes=12)
 
     #Then, we loop over the data a couple of times with periodic evals.
     #The data needs to be shuffled.
     objs, labels = (obj[0], obj[1])#unison_shuffled_copies(obj[0], obj[1])
 
     net = hk.without_apply_rng(hk.transform(net_fn))
-
-    print("pre init")
     # Initialize the network.
-    params = net.init(jax.random.PRNGKey(42), np.expand_dims(prep_data(objs[0]), axis=0))
-
-    print("initted")
+    params = net.init(jax.random.PRNGKey(42), np.expand_dims(prep_data(objs[0], sym), axis=0))
 
     #modify this to look for a current parameter set.
     if exists("ccnn.params"):
@@ -203,93 +202,102 @@ def main(obj):
     opt_init, opt_update = optax.adam(1e-4)
     opt_state = opt_init(params)
 
-    compute_loss_fn = functools.partial(compute_loss, net=net)
-    # We jit the computation of our loss, since this is the main computation.
-    # Using jax.jit means that we will use a single accelerator. If you want
-    # to use more than 1 accelerator, use jax.pmap. More information can be
-    # found in the jax documentation.
-    compute_loss_fn = jax.jit(jax.value_and_grad(
-        compute_loss_fn, has_aux=True))
-
+    compute_loss_fn = jax.jit(jax.value_and_grad(functools.partial(compute_loss, net=net), has_aux=True))
 
     testObjects = objs[:(math.floor(len(labels) / 10))]
     testLabels = labels[:(math.floor(len(labels) / 10))]
     trainingObjects = objs[math.floor(len(labels) / 10):]
     trainingLabels = labels[math.floor(len(labels) / 10):]
 
-    print("starting training")
-    batchSize = 10
-    num = len(trainingLabels) - batchSize
+    print("starting training", flush=True)
 
     #I try to fix the performance issue by reshaping the list ahead of time, then measuring its in-app performance
     #Time the prep stage
 
-    print("point 1")
-
-    accumChunks = [0]*math.floor(num / batchSize)
-
-    for i in range(math.floor(num / batchSize)):
-      accumChunks[i] = (trainingObjects[batchSize * i:batchSize * (i+1)], 
-                        jnp.array(trainingLabels[batchSize * i:batchSize * (i+1)]))
-
-    print("point 2")
-
-    #The following functions are wrappers for the multiprocessing modules to play with:
-    def preprocess_chunk(data_obs):
-      return np.array(list(pool.map(prep_data, data_obs[0]))), data_obs[1]
+    print("point 1", flush=True)
     
     #must be called on single thread!!!
-    def final_process_data(data_objs, ii):
+    def final_process_data(x_data, y_data, ii):
       nonlocal opt_state
       nonlocal params
       nonlocal testObjects
       nonlocal testLabels
-      (loss, acc), grad = compute_loss_fn(params, data_objs[0], data_objs[1])
+      (loss, acc), grad = compute_loss_fn(params, x_data, y_data)
       updates, opt_state = opt_update(grad, opt_state, params)
       params = optax.apply_updates(params, updates)
 
       if ii % 1 == 0:
-        print(f'step: {ii}, loss: {loss}, acc: {acc}')
-      #evaluate needs the same type of speed-up to be reasonable
-      #if ii > 0 and ii % 100 == 0:
-        #evaluate(testObjects, testLabels, params)
+        print(f'step: {ii}, loss: {loss}, acc: {acc}', flush=True)
 
     try:
-      """
-      The following is getting rearranged as a multiprocessed array. 
-      Testing takes a while so don't do it too frequently
-      It's dif
-      """
-        
-      with concurrent.futures.ThreadPoolExecutor(max_workers=20) as preprocessing_executor:
-        processed_chunks = 0
+      manager = Manager()
+      results_queue = manager.Queue()
 
-        preprocess_future = [preprocessing_executor.submit(preprocess_chunk, chunk) for chunk in accumChunks]
+      # Create a pool of worker processes
+      pool = Pool(processes=12)
 
-        print("preprocessors scheduled")
+      # Asynchronously apply the worker_function to each data item
+      print("pre-initing workers", flush=True)
+      for index, data_item in enumerate(trainingObjects):
+          pool.apply_async(ordered_process, args=(index, data_item,sym), callback=results_queue.put)
+      
+      print("copmutations appended", flush=True)
 
-        #Potentially bad if the preprocessing starts outpacing the ML...
-        for future in concurrent.futures.as_completed(preprocess_future):
-          preprocessed_batch = future.result()
-          final_process_data(preprocessed_batch, processed_chunks)
-          processed_chunks += 1
+      #As the last async thing dropped, we have None. We can lose up to #processes of data, but whatever. Once this is read, exit
+      pool.apply_async(lambda a: None, args=(None,), callback=results_queue.put)
+
+      batch = []
+
+      ii = 0
+      while True:
+          result = results_queue.get()  # This will block until a result is available
+          ii += 1
+
+          if ii % 12 == 0:
+             print("Batched: ", ii, flush=True)
+
+          if result is None:
+              print("ob no", flush=True)
+              # If there are remaining items in the batch when a sentinel is encountered, process them
+              if batch:
+                  x_data = [i[1] for i in batch]
+                  y_data = [trainingLabels[i[0]] for i in batch]
+
+                  
+                  final_process_data(x_data, y_data, 1)
+                  batch.clear()
+              break
+
+          batch.append(result)
+
+          if len(batch) == 6:
+              x_data = np.array([i[1] for i in batch])
+              y_data = np.array([trainingLabels[i[0]] for i in batch])
+
+              
+              final_process_data(x_data, y_data, 1)
+              batch.clear()
+      
+      pool.close()
+      pool.join()
 
     except KeyboardInterrupt:
       file = open('model-dump-5.params', 'wb')
       pickle.dump(params, file)
       file.close()
-      print("Failsafe")
+      print("Failsafe", flush=True)
       exit()
 
     evaluate(testObjects, testLabels, params)
-    print('Training finished')
+    print('Training finished', flush=True)
 
     #If training is finished, just dump everything, so I can use it again, or start from the new stuff.
 
     file = open('trained-model-5.params', 'wb')
     pickle.dump(params, file)
     file.close()
-    print("Bye")
+    print("Bye", flush=True)
     exit()
 
-cmd_line(main, "ccnn")
+if __name__ == '__main__':
+    cmd_line(main, "ccnn")
